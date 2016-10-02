e2ababde3162334355cdcd5fd72145ccf39c398c
Quinton Helton <quinton.helton@mavs.uta.edu>
2016-10-02 16:03:38 -0500

    added Battery class

---
+++ Battery.h
@@ -0,0 +1,10 @@
+#include "Robot_Part.h"
+
+class Battery : public Robot_Part
+{
+private:
+	double energy;
+public:
+	Battery(string n, int pn, double w, double c, string d, double e);
+	double get_energy_contained();
+};
