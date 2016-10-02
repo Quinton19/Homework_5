ed858d2fe46ceb332d0bf51a97d57a3510784bac
Quinton Helton <quinton.helton@mavs.uta.edu>
2016-10-02 15:52:00 -0500

    added Locomotor class

---
+++ Locomotor.h
@@ -0,0 +1,12 @@
+#include "Robot_Part.h"
+
+class Locomotor : public Robot_Part
+{
+private:
+	int max_speed;
+	int power_consumed;
+public:
+	Locomotor(string n, int pn, double w, double c, string d, int ms, int pc);
+	int get_max_speed();
+	int get_power_consumed();
+};
